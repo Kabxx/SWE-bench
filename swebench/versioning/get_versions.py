f7b6a40e34d85a797885204bbf6ec633cd3aadde
Xihua Wang <2201174299@qq.com>
2024-11-11 15:49:14 +0800

    Refactor output format in get_versions.py from JSON array to JSON Lines (JSONL)

--- swebench/versioning/get_versions.py
+++ swebench/versioning/get_versions.py
@@ -235,7 +235,10 @@ def merge_results(instances_path: str, repo_prefix: str, output_dir: str = None)
     if output_dir is not None:
         instances_path_new = os.path.join(output_dir, instances_path_new)
     with open(f"{instances_path_new}", "w") as f:
-        json.dump(merged, fp=f)
+        # json.dump(merged, fp=f)
+        for instance in merged:
+            f.write(json.dumps(instance))
+            f.write('\n')
     logger.info(f"Saved merged results to {instances_path_new} ({len(merged)} instances)")
     return len(merged)
 
